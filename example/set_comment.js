6ea85265139b3c662522e0671d3ad74230389b6d
Martin Rode <martin.rode@programmfabrik.de>
2020-08-04 18:23:55 +0200

    example: adopted to changed interface to fylr
    For the object callbacks, there is no longer a separate info.json. We keep that only for extension plugins.

--- example/set_comment.js
+++ example/set_comment.js
@@ -1,18 +1,4 @@
-var info = JSON.parse(process.argv[2]);
-
-var comment = (info.request && info.request.query && info.request.query.comment) || info.comment
-
-if (comment == undefined) {
-    // console.error(JSON.stringify(info,"","    "))
-    // Output feeback on STDOUT
-    console.log(JSON.stringify(
-        {
-            status_code: 304
-        }
-    ))
-    process.exit(1);
-}
-
+const fs = require('fs');
 
 let input = '';
 process.stdin.on('data', d => {
@@ -27,18 +13,31 @@ process.stdin.on('data', d => {
 process.stdin.on('end', () => {
     let data;
     try {
+        fs.writeFileSync('/tmp/post-data', input);
         data = JSON.parse(input);
     } catch(e) {
         console.error(`Could not parse input string into JSON array: ${e.message}`, e.stack);
         process.exit(1);
     }
 
-    // add more info from the config value
-    if (info.config.system.plugin_fylr_example_comment.value) {
-        comment = comment + " " + info.config.system.plugin_fylr_example_comment.value
+    var comment = (data.info.request && data.info.request.query && data.info.request.query.comment) || data.info.comment
+    if (comment == undefined) {
+        // console.error(JSON.stringify(info,"","    "))
+        // Output feeback on STDOUT
+        console.log(JSON.stringify(
+            {
+                status_code: 304
+            }
+        ))
+        process.exit(1);
     }
 
-    data.forEach((d, i) => {
+    // add more info from the config value
+    if (data.info.config.system.plugin_fylr_example_comment.value) {
+        comment = comment + " " + data.info.config.system.plugin_fylr_example_comment.value
+    }
+
+    data.objects.forEach((d, i) => {
         if (d[d._objecttype]._version === 1) {
             return
         }
@@ -58,6 +57,5 @@ process.stdin.on('end', () => {
 
     var myArgs = process.argv.slice(0);
     console.error('myArgs: ', myArgs);
-
     console.log(JSON.stringify(data));
 });
