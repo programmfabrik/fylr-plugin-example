b5199be541a8c3497b070385620fbabd79e027dd
Martin Rode <martin.rode@programmfabrik.de>
2021-04-21 16:32:06 +0200

    export: removed md5 checksum code, added "done" state

--- example/export_demo.js
+++ example/export_demo.js
@@ -2,36 +2,6 @@ const fs = require('fs');
 const https = require('https');
 const crypto = require('crypto');
 
-
-
-const getMD5FromURL = (url) => {
-    const md5sum = crypto.createHash('md5');
-    console.error("url", url)
-    return new Promise(
-        (resolve, reject) => {
-            const req = https.request(url, options, (res) => {
-                res.on('end', () => {
-                    console.error("end")
-                    resolve(md5sum.digest('hex'));
-                });
-                res.on('error', (err) => {
-                    console.error("end", err)
-                    reject(err);
-                });
-                res.on('data', (d) => {
-                    console.error("data", d)
-                    md5sum.update(d);
-                });
-            });
-            req.on('error', (err) => {
-                console.error("err", err)
-                reject(err);
-            });
-            req.end();
-        }
-        );
-}
-
 if (process.argv.length < 3) {
     console.error(`Wrong number of parameters. Usage: "node export_demo.js <info>"`)
     process.exit(1);
@@ -56,27 +26,8 @@ try {
 console.error(info.api_callback);
 
 if (info.plugin_action == "produce?infos.json") {
-    let ps = []
-    for (let f of info.export._files) {
-        let url = info.api_callback.url+"/export/"+info.export.export._id+"/file/"+f.path
-        console.error(url, f.export_file_internal.hidden)
-        if (f.export_file_internal.hidden) {
-            ps.push(getMD5FromURL(url)
-            .catch(err => {
-                console.error("error", err)
-            })
-            .then( md5 => {
-                console.error("url done", md5)
-                f.md5 = md5
-            }))
-        }
-    }
-    console.error("test")
-    Promise.all(ps).then(() => {
-        console.log(JSON.stringify(info.export._files, "", "    "))
-        process.exit(0)
-    })
-    while(true) {}
+    console.log(JSON.stringify(info.export._files, "", "    "))
+    process.exit(0);
 }
 
 for (let f of info.export._files) {
@@ -110,9 +61,8 @@ delete(info.export._log)
 // console.error(JSON.stringify(info.export, "", "    "))
 console.error("files", info.export._files.length, "version", info.export.export._version)
 
+info.export._state = "done"
+info.export._plugin_protocol = "Add a protocol here\nWith multilines like\nthis"
 
 // write back modified export json
 console.log(JSON.stringify(info.export, "", "    "))
-
-
-
