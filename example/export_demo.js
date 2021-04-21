1c5dff1372391afc8e1fd355e9ab7b8ca95ce534
Unai Garcia <unai.garcia@programmfabrik.de>
2021-04-21 17:41:23 +0200

    made some synchronoud out of the asynchronous, some node tunings too

--- example/export_demo.js
+++ example/export_demo.js
@@ -1,35 +1,21 @@
 const fs = require('fs');
-const https = require('https');
+const https = require('http');
 const crypto = require('crypto');
 
-
-
-const getMD5FromURL = (url) => {
+const getMD5FromURL = async (url) => {
     const md5sum = crypto.createHash('md5');
-    console.error("url", url)
     return new Promise(
         (resolve, reject) => {
-            const req = https.request(url, options, (res) => {
-                res.on('end', () => {
-                    console.error("end")
-                    resolve(md5sum.digest('hex'));
-                });
-                res.on('error', (err) => {
-                    console.error("end", err)
-                    reject(err);
-                });
-                res.on('data', (d) => {
-                    console.error("data", d)
-                    md5sum.update(d);
-                });
+            const req = https.request(url, {timeout: 10000},(res) => {
+                res.on('end', () => resolve(md5sum.digest('hex')));
+                res.on('error', reject);
+                res.on('data', (d) => md5sum.update(d));
             });
-            req.on('error', (err) => {
-                console.error("err", err)
-                reject(err);
-            });
+            req.on('timeout', () => reject(new Error("TIMEOUT")));
+            req.on('error', reject);
             req.end();
         }
-        );
+    );
 }
 
 if (process.argv.length < 3) {
@@ -53,66 +39,56 @@ try {
     process.exit(1);
 }
 
-console.error(info.api_callback);
-
-if (info.plugin_action == "produce?infos.json") {
-    let ps = []
-    for (let f of info.export._files) {
-        let url = info.api_callback.url+"/export/"+info.export.export._id+"/file/"+f.path
-        console.error(url, f.export_file_internal.hidden)
-        if (f.export_file_internal.hidden) {
-            ps.push(getMD5FromURL(url)
-            .catch(err => {
-                console.error("error", err)
-            })
-            .then( md5 => {
-                console.error("url done", md5)
-                f.md5 = md5
-            }))
+(async() => {
+    if (info.plugin_action == "produce?infos.json") {
+        for (let i = 0, j = info.export._files.length; i < j; i++) {
+            const f = info.export._files[i];
+            let url = info.api_callback.url+"/export/"+info.export.export._id+"/file/"+f.path
+            if (f.export_file_internal.hidden && f.export_file_internal.plugin_action !== "produce?infos.json") {
+                try {
+                    info.export._files[i].md5 = await getMD5FromURL(url);
+                    console.error('MD5: ' + info.export._files[i].md5);
+                } catch (e) {
+                    console.error(e);
+                }
+            }
         }
-    }
-    console.error("test")
-    Promise.all(ps).then(() => {
         console.log(JSON.stringify(info.export._files, "", "    "))
         process.exit(0)
-    })
-    while(true) {}
-}
-
-for (let f of info.export._files) {
-    console.error(f)
-    if (f.export_file_internal.file_id > 0) {
-        // do not hide regular download files
-    } else {
-        // hide all data files
-        f.export_file_internal.hidden = true
-        console.error("hiding file", f.path)
     }
-}
 
-info.export._files.push({
-    "path": "files/infos.json",
-    "format": "application/json",
-    "export_file_internal": {
-        "path": "files/infos.json",
-        "format": "application/json",
-        "plugin_action": "produce?infos.json",
-        "info": {
+    for (let f of info.export._files) {
+        console.error(f)
+        if (f.export_file_internal.file_id > 0) {
+            // do not hide regular download files
+        } else {
+            // hide all data files
+            f.export_file_internal.hidden = true
+            console.error("hiding file", f.path)
         }
     }
-})
 
-// add a new file which our plugin produces / provides
-info.export.export._version++
+    info.export._files.push({
+        "path": "files/infos.json",
+        "format": "application/json",
+        "export_file_internal": {
+            "path": "files/infos.json",
+            "format": "application/json",
+            "plugin_action": "produce?infos.json",
+            "info": {
+            }
+        }
+    })
 
-delete(info.export._log)
+    // add a new file which our plugin produces / provides
+    info.export.export._version++
 
-// console.error(JSON.stringify(info.export, "", "    "))
-console.error("files", info.export._files.length, "version", info.export.export._version)
+    delete(info.export._log)
 
+    // console.error(JSON.stringify(info.export, "", "    "))
+    console.error("files", info.export._files.length, "version", info.export.export._version)
 
-// write back modified export json
-console.log(JSON.stringify(info.export, "", "    "))
+    // write back modified export json
+    console.log(JSON.stringify(info.export, "", "    "))
 
-
-
+})();
