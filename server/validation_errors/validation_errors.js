ac3bab23bfbd9c25b62a3f26a2314141deffa00a
Francisco Marrero <admin@novax.pro>
2023-07-18 14:35:22 +0100

    Initial commit for validation_error example on the plugins example.

---
+++ server/validation_errors/validation_errors.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+let input = '';
+process.stdin.on('data', d => {
+    try {
+        input += d.toString();
+    } catch(e) {
+        console.error(`Could not read input into string: ${e.message}`, e.stack);
+        process.exit(1);
+    }
+});
+
+
+process.stdin.on('end', () => {
+    console.log(JSON.stringify(
+        {
+            "code": "validation.plugin.error",
+            "statuscode": 400,
+        }
+    ));
+    console.error("This error goes to STDERR")
+    process.exit(400);
+});
