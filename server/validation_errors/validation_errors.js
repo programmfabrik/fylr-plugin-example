8fd916ae42c3ef59e5156a213501822f0e0d3aaf
Francisco Marrero <admin@novax.pro>
2023-08-02 20:52:07 +0100

    Updates validation error example

--- server/validation_errors/validation_errors.js
+++ server/validation_errors/validation_errors.js
@@ -13,14 +13,50 @@ process.stdin.on('data', d => {
 
 
 process.stdin.on('end', () => {
+    // We get the objects being saved from input
+    const objects = JSON.parse(input)?.objects;
+    if (!objects || !objects.length > 0) {
+        process.exit(1);
+    }
+
+    const objecttype = objects[0]["_objecttype"];
+    let problemsCount = 0;
+    const errors = objects.map(o => {
+        let e = [];
+        for (const fieldName in o[objecttype]) {
+            if (fieldName.startsWith("_")) {
+                continue;
+            }
+            const fieldValue = o[objecttype][fieldName];
+            if (fieldValue === null || fieldValue === undefined || fieldValue === "") {
+                continue;
+            }
+            e.push({
+                "field": fieldName,
+                "message": `This is a dummy error message for ${fieldName} with value ${fieldValue}, we only accept empty values here...`,
+                "parameters": {
+                    a: "a",
+                    b: "b",
+                }
+            });
+            problemsCount++;
+        }
+        return e;
+    });
+
+    if (problemsCount === 0) {
+        console.log(JSON.stringify({"objects": []}));
+        console.error("Validation ok");
+        process.exit(0);
+        return
+    }
+
     console.log(JSON.stringify(
         {
             "code": "validation.plugin.error",
             "statuscode": 400,
             "parameters": {
-                "henk": "torsten",
-                "a": 4,
-                "b": [ 1, 2, 3 ]
+                "problems": errors,
             }
         }
     ));
