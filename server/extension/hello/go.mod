c28ebed26dac59df0028c617d31a5ec554b51cab
Martin Rode <martin.rode@programmfabrik.de>
2024-10-21 18:29:03 +0200

    Added custom plugin mapping demo

--- server/extension/hello/go.mod
+++ server/extension/hello/go.mod
@@ -5,16 +5,17 @@ go 1.20
 require github.com/programmfabrik/golib v0.0.0-20230503055439-c28ab3558111
 
 require (
-	github.com/antchfx/xmlquery v1.3.9 // indirect
-	github.com/antchfx/xpath v1.2.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
-	github.com/gofrs/uuid v4.0.0+incompatible // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/antchfx/xmlquery v1.4.1 // indirect
+	github.com/antchfx/xpath v1.3.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/yuin/goldmark v1.4.0 // indirect
-	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/yuin/goldmark v1.7.4 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
--- server/recipe/meta.js
+++ server/recipe/meta.js
@@ -9,6 +9,64 @@ fs.writeFileSync(fn, JSON.stringify({
     "Zonk": {
         "Honk": "henk"
     },
+    "plugin-example": {
+        "chat-gpt": {
+            "fields": {
+                "filename": "trabant.jpg",
+                "description": {
+                    "de-DE": "Das Bild zeigt ein Gemälde eines weißen Trabant-Autos, das durch eine Wand bricht. Die Illusion von zerbrochenem Putz und Ziegeln verstärkt den Eindruck einer Durchbrechung. Auf dem Nummernschild steht 'NOV 9-89', ein Hinweis auf den Tag des Mauerfalls in Berlin. Das Kunstwerk ist von Birgit Kinder signiert.",
+                    "en-US": "The image shows a painting of a white Trabant car breaking through a wall. The illusion of broken plaster and bricks enhances the impression of the car crashing through. The license plate reads 'NOV 9-89', referencing the date of the Berlin Wall's fall. The artwork is signed by Birgit Kinder."
+                },
+                "keywords": [
+                    {
+                        "de-DE": "Trabant",
+                        "en-US": "Trabant"
+                    },
+                    {
+                        "de-DE": "Berlin",
+                        "en-US": "Berlin"
+                    },
+                    {
+                        "de-DE": "Mauer",
+                        "en-US": "Wall"
+                    },
+                    {
+                        "de-DE": "Kunst",
+                        "en-US": "Art"
+                    },
+                    {
+                        "de-DE": "Wand",
+                        "en-US": "Crash"
+                    },
+                    {
+                        "de-DE": "Durchbruch",
+                        "en-US": "Breakthrough"
+                    },
+                    {
+                        "de-DE": "Mauerfall",
+                        "en-US": "Wall Fall"
+                    },
+                    {
+                        "de-DE": "Illusion",
+                        "en-US": "Illusion"
+                    },
+                    {
+                        "de-DE": "Birgit Kinder",
+                        "en-US": "Birgit Kinder"
+                    },
+                    {
+                        "de-DE": "Auto",
+                        "en-US": "Car"
+                    }
+                ],
+                "title": {
+                    "de-DE": "Auto durch die Mauer",
+                    "en-US": "Car Breaking Through Wall"
+                }
+            },
+            "timestamp": "2024-10-18T13:15:11+02:00"
+        }
+    },
     "_fulltext": {
         "text": ["horst sieht torsten"]
     },
